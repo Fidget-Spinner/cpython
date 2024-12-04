02d3a4d24c62c57fcca359170090b59e75760808
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2024-12-04 23:01:35 +0800

    Fix tests

--- Lib/test/test_capi/test_opt.py
+++ Lib/test/test_capi/test_opt.py
@@ -1491,7 +1491,7 @@ class TestUopsOptimization(unittest.TestCase):
 
 
         res, ex = self._run_with_optimizer(thing, 1)
-        self.assertEqual(res, 19)
+        self.assertEqual(res, 4095)
         self.assertIsNotNone(ex)
         self.assertEqual(list(iter_opnames(ex)).count("_POP_TOP"), 0)
         self.assertTrue(ex.is_valid())
@@ -1505,7 +1505,7 @@ class TestUopsOptimization(unittest.TestCase):
 
 
         res, ex = self._run_with_optimizer(thing, 1)
-        self.assertEqual(res, 19)
+        self.assertEqual(res, 4095)
         self.assertIsNotNone(ex)
         self.assertEqual(list(iter_opnames(ex)).count("_LOAD_FAST_1"), 0)
         self.assertTrue(ex.is_valid())
@@ -1523,7 +1523,6 @@ class TestUopsOptimization(unittest.TestCase):
         ex = get_first_executor(testfunc)
         self.assertIsNotNone(ex)
         uops = get_opnames(ex)
-        self.assertIn("_PUSH_FRAME", uops)
         # Strength reduced version
         self.assertIn("_CHECK_FUNCTION_VERSION_INLINE", uops)
         self.assertNotIn("_CHECK_FUNCTION_VERSION", uops)
